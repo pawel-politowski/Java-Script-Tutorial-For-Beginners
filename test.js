820248c8c1f5e4e66043f90f41a5bfd67f1d23c7
Paweł Politowski <matelly@poczta.onet.pl>
2020-01-08 07:44:35 +0100

    Modified test.js by adding else if statements

--- test.js
+++ test.js
@@ -1,9 +1,19 @@
-var youLikeMeat = false;
-var myNum = 8;
+var myAge = 29;
 
- if (myNum == 10){
-     document.write("myNum is equal to 10");
- } else{
-     document.write("myNum is not equal to 10");
- }
+if (myAge > 30){
+
+    document.write("you are over 30!");
+
+} else if(myAge > 20){
+
+    document.write("you are over 20!");
+
+} else if(myAge > 10){
+
+    document.write("you are over 10!");
+
+} else{
+
+    document.write("you are not over 10!");
+}
  
