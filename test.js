1aada8cbe59b58b05b853737ca571b371661c5cd
Paweł Politowski <matelly@poczta.onet.pl>
2020-01-08 18:53:10 +0100

    Modified test.jss

--- test.js
+++ test.js
@@ -1,18 +1,4 @@
- function getAverage (a,b){
+var a = 5;
+var b = 5;
 
-    var average = (a + b) / 2; //local variable
-    console.log(average);
-    return average;
-
- }
-
- var myResult = getAverage(7,11); // global variable
- 
-
- function logResult(){
-
-    console.log("the average is " + myResult + " inside the function");
- }
-
- logResult();
-
+console.log(a + b);
