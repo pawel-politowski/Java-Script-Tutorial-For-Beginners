d7a2cede2096928b32d4647cf15ae81cdc7f4563
Paweł Politowski <matelly@poczta.onet.pl>
2020-01-08 07:36:57 +0100

    Modified test.js by adding some if statements

--- test.js
+++ test.js
@@ -1,5 +1,9 @@
-// the first alert
-alert("Hello, World!");
+var youLikeMeat = false;
+var myNum = 8;
 
-// the second alert
-alert("Hello, World!");
+ if (myNum == 10){
+     document.write("myNum is equal to 10");
+ } else{
+     document.write("myNum is not equal to 10");
+ }
+ 
