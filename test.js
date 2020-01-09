d96d4d68b71f1f25acd8620a7ba9549944074532
Paweł Politowski <matelly@poczta.onet.pl>
2020-01-09 07:51:46 +0100

    Modified test.jss, tried out some data

--- test.js
+++ test.js
@@ -1,22 +1,2 @@
-
-var Car = function(maxSpeed, driver){
-
-    this.maxSpeed = maxSpeed;
-    this.driver = driver;
-    this.drive = function(speed, time){
-        console.log(speed * time);
-    };
-    this.logDriver = function(){
-        console.log("driver name is " + this.driver);
-    };
-}
-
-var myCar = new Car(70, "Net Man");
-var myCar2 = new Car(50, "Net");
-var myCar3 = new Car(40, "Man");
-var myCar4 = new Car(90, "Elo");
-
-myCar.drive(30, 5);
-
-
-
+var myDate = new Date(1943, 12, 21, 12, 23, 45);
+console.log(myDate);
