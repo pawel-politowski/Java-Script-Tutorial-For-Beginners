dbd2e4e684014abb1a8298db8d936261f83e6ca5
Paweł Politowski <matelly@poczta.onet.pl>
2020-01-09 07:38:45 +0100

    Modified test.jss

--- test.js
+++ test.js
@@ -1,19 +1,22 @@
-var myCar2 = {
 
-    maxSpeed: 70,
-    driver: "Net Ninja",
-    drive: function(speed, time){
+var Car = function(maxSpeed, driver){
+
+    this.maxSpeed = maxSpeed;
+    this.driver = driver;
+    this.drive = function(speed, time){
         console.log(speed * time);
-    },
-    logDriver: function(){
+    };
+    this.logDriver = function(){
         console.log("driver name is " + this.driver);
-    }
-};
+    };
+}
 
+var myCar = new Car(70, "Net Man");
+var myCar2 = new Car(50, "Net");
+var myCar3 = new Car(40, "Man");
+var myCar4 = new Car(90, "Elo");
 
+myCar.drive(30, 5);
 
-myCar2.logDriver();
-console.log(myCar2.maxSpeed);
-myCar2.drive(50, 3);
 
 
