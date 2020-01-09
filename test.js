4dcbdf90656127eda2eb773176950624decb6754
Paweł Politowski <matelly@poczta.onet.pl>
2020-01-09 12:03:23 +0100

    Created style.css, modified index.html and test.jss

--- test.js
+++ test.js
@@ -1,2 +1,16 @@
-var myDate = new Date(1943, 12, 21, 12, 23, 45);
-console.log(myDate);
+var content = document.getElementById("content");
+var button = document.getElementById("show-more");
+
+button.onclick = function(){
+    
+    if(content.className == "open"){
+        //shrink the box
+        content.className = "";
+        button.innerHTML = "Show More";
+    } else{
+        //expand the box
+        content.className = "open";
+        button.innerHTML = "Show Less";
+    }
+}
+
