dc6b850b039610136f925ee2a4a965c310ea0816
Paweł Politowski <matelly@poczta.onet.pl>
2020-01-09 07:22:38 +0100

    Modified test.jss by adding some this object

--- test.js
+++ test.js
@@ -1,25 +1,18 @@
-var myArray = new Array();
-myArray[0] = 8;
-myArray[1] = "hello";
-
-var myCar = new Object();
-myCar.maxSpeed = 50;
-myCar.driver = "pawel";
-myCar.drive = function() {
-    console.log("now driving");
-};
-
-myCar.drive();
-
 var myCar2 = {
 
     maxSpeed: 70,
     driver: "Net Ninja",
     drive: function(speed, time){
         console.log(speed * time);
+    },
+    logDriver: function(){
+        console.log("driver name is " + this.driver);
     }
 };
 
+
+
+myCar2.logDriver();
 console.log(myCar2.maxSpeed);
 myCar2.drive(50, 3);
 
